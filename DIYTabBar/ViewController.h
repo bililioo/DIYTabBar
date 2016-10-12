dba760ec6472e20ae1f0f923c6a557ea549be422
chenbin <990978664@qq.com>
2016-10-12 16:59:02 +0800

    first commit

---
+++ DIYTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DIYTabBar
+//
+//  Created by Bin on 16/10/12.
+//  Copyright © 2016年 urun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DIYTabBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DIYTabBar
+//
+//  Created by Bin on 16/10/12.
+//  Copyright © 2016年 urun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
